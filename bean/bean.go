ff1d437a1cde511a895dad69ec7f5b7ba3f62a33
vishal bihani <vishal@devtron.ai>
2023-03-07 01:08:26 +0530

    make REST default

--- bean/bean.go
+++ bean/bean.go
@@ -1,5 +1,5 @@
 package bean
 
 type GitSensorProtocolConfig struct {
-	Protocol string `env:"GIT_SENSOR_PROTOCOL" envDefault:"GRPC"`
+	Protocol string `env:"GIT_SENSOR_PROTOCOL" envDefault:"REST"`
 }
